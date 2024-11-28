e4abaee09252ade829857953ac9ae012f96fe461
Rushil Ambati <rush2369.21@gmail.com>
2024-11-28 00:01:25 +0000

    implement counter program

--- ebpf-actors-ebpf/src/main.rs
+++ ebpf-actors-ebpf/src/main.rs
@@ -1,8 +1,23 @@
 #![no_std]
 #![no_main]
 
-use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
+use core::mem;
+
+use aya_ebpf::{
+    bindings::xdp_action,
+    macros::{map, xdp},
+    maps::HashMap,
+    programs::XdpContext,
+};
 use aya_log_ebpf::info;
+use network_types::{
+    eth::{EthHdr, EtherType},
+    ip::{IpProto, Ipv4Hdr},
+    udp::UdpHdr,
+};
+
+#[map]
+static COUNTERS: HashMap<u32, u32> = HashMap::<u32, u32>::with_max_entries(1024, 0);
 
 #[xdp]
 pub fn ebpf_actors(ctx: XdpContext) -> u32 {
@@ -12,9 +27,65 @@ pub fn ebpf_actors(ctx: XdpContext) -> u32 {
     }
 }
 
-fn try_ebpf_actors(ctx: XdpContext) -> Result<u32, u32> {
-    info!(&ctx, "received a packet");
-    Ok(xdp_action::XDP_PASS)
+#[inline(always)]
+fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(());
+    }
+
+    Ok((start + offset) as *const T)
+}
+
+fn try_ebpf_actors(ctx: XdpContext) -> Result<u32, ()> {
+    let ethhdr: *const EthHdr = ptr_at(&ctx, 0)?;
+    match unsafe { (*ethhdr).ether_type } {
+        EtherType::Ipv4 => {}
+        _ => return Ok(xdp_action::XDP_PASS),
+    }
+
+    let ipv4hdr: *const Ipv4Hdr = ptr_at(&ctx, EthHdr::LEN)?;
+    let source_addr = u32::from_be(unsafe { (*ipv4hdr).src_addr });
+
+    let udphdr: *const UdpHdr = match unsafe { (*ipv4hdr).proto } {
+        IpProto::Udp => ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?,
+        _ => return Ok(xdp_action::XDP_PASS),
+    };
+
+    let dest_port = u16::from_be(unsafe { (*udphdr).dest });
+    if dest_port != 1337 {
+        return Ok(xdp_action::XDP_PASS);
+    }
+
+    let payload_offset = EthHdr::LEN + Ipv4Hdr::LEN + UdpHdr::LEN;
+    let payload_len = ctx.data_end() - ctx.data() - payload_offset;
+
+    let inc = b"INC";
+
+    if payload_len < inc.len() + 1 {
+        return Ok(xdp_action::XDP_PASS);
+    }
+
+    for i in 0..inc.len() {
+        let byte: *const u8 = ptr_at(&ctx, payload_offset + i)?;
+        if unsafe { *byte } != inc[i] {
+            return Ok(xdp_action::XDP_PASS);
+        }
+    }
+
+    let counter = unsafe {COUNTERS.get(&source_addr).unwrap_or(&0) };
+    let new_counter = *counter + 1;
+    let _ = COUNTERS.insert(&source_addr, &new_counter, 0);
+
+    info!(
+        &ctx,
+        "Received INC from IP: {:i}, count: {}", source_addr, new_counter
+    );
+
+    Ok(xdp_action::XDP_DROP)
 }
 
 #[cfg(not(test))]
--- ebpf-actors/src/main.rs
+++ ebpf-actors/src/main.rs
@@ -1,14 +1,65 @@
 use anyhow::Context as _;
+use aya::maps::{HashMap, MapData};
 use aya::programs::{Xdp, XdpFlags};
 use clap::Parser;
 #[rustfmt::skip]
 use log::{debug, warn};
-use tokio::signal;
+use std::net::{IpAddr, SocketAddr};
+use std::{env, io};
+use tokio::net::UdpSocket;
+use tokio::{signal, task};
 
 #[derive(Debug, Parser)]
 struct Opt {
     #[clap(short, long, default_value = "eth0")]
     iface: String,
+}
+
+struct Server {
+    socket: UdpSocket,
+    buf: Vec<u8>,
+    to_send: Option<(usize, SocketAddr)>,
+    map: HashMap<MapData, u32, u32>,
+}
+
+impl Server {
+    async fn run(self) -> Result<(), io::Error> {
+        let Server {
+            socket,
+            mut buf,
+            mut to_send,
+            mut map,
+        } = self;
+
+        loop {
+            // First we check to see if there's a message we need to echo back.
+            // If so then we try to send it back to the original source, waiting
+            // until it's writable and we're able to do so.
+            if let Some((size, peer)) = to_send {
+                if let IpAddr::V4(ipv4) = peer.ip() {
+                    let ipv4_as_u32: u32 = ipv4.into();
+
+                    let msg = std::str::from_utf8(&buf[..size-1]).unwrap();
+                    match msg {
+                        "GET" => {
+                            let count = map.get(&ipv4_as_u32, 0).unwrap_or(0);
+                            // send the count back to the client
+                            socket.send_to((count.to_string() + "\n").as_bytes(), &peer).await?;
+                        }
+                        "RST" => {
+                            println!("[USERSPACE] Resetting counter for IP: {:?}", ipv4);
+                            map.insert(&ipv4_as_u32, 0, 0).unwrap();
+                        }
+                        _ => (),
+                    }
+                }
+            }
+
+            // If we're here then `to_send` is `None`, so we take a look for the
+            // next message we're going to echo back.
+            to_send = Some(socket.recv_from(&mut buf).await?);
+        }
+    }
 }
 
 #[tokio::main]
@@ -46,10 +97,33 @@ async fn main() -> anyhow::Result<()> {
     program.attach(&iface, XdpFlags::default())
         .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
 
+    let count_map: HashMap<_, u32, u32> = HashMap::try_from(
+        ebpf.take_map("COUNTERS").context("failed to take COUNTERS map")?,
+    )?;
+
+    // Userspace echo server
+    let addr = "192.168.1.151:1337";
+    let socket = UdpSocket::bind(&addr).await?;
+    println!("Listening on: {}", socket.local_addr()?);
+    let server = Server {
+        socket,
+        buf: vec![0; 1024],
+        to_send: None,
+        map: count_map,
+    };
+    // Spawn the echo server in a new asynchronous task
+    let server_handle = task::spawn(async move {
+        if let Err(e) = server.run().await {
+            eprintln!("Server error: {}", e);
+        }
+    });
+
     let ctrl_c = signal::ctrl_c();
     println!("Waiting for Ctrl-C...");
     ctrl_c.await?;
     println!("Exiting...");
+
+    server_handle.abort();
 
     Ok(())
 }
