592e6ae35008f733476bb1f6687f6c9c2619c366
Rushil Ambati <rush2369.21@gmail.com>
2025-04-24 03:05:42 +0100

    [wip] abd writer scaffolding

---
+++ abd-bpf-ebpf/src/abd_writer.rs
@@ -0,0 +1,56 @@
+#![no_std]
+#![no_main]
+
+use aya_ebpf::{
+    bindings::{TC_ACT_PIPE, TC_ACT_SHOT},
+    macros::{classifier, map},
+    maps::HashMap,
+    programs::TcContext,
+};
+use aya_log_ebpf::info;
+use network_types::{
+    eth::{EthHdr, EtherType},
+    ip::Ipv4Hdr,
+};
+
+#[map]
+static BLOCKLIST: HashMap<u32, u32> = HashMap::with_max_entries(1024, 0);
+
+#[classifier]
+pub fn abd_writer(ctx: TcContext) -> i32 {
+    match try_abd_writer(ctx) {
+        Ok(ret) => ret,
+        Err(_) => TC_ACT_SHOT,
+    }
+}
+
+fn block_ip(address: u32) -> bool {
+    unsafe { BLOCKLIST.get(&address).is_some() }
+}
+
+fn try_abd_writer(ctx: TcContext) -> Result<i32, ()> {
+    let ethhdr: EthHdr = ctx.load(0).map_err(|_| ())?;
+    match ethhdr.ether_type {
+        EtherType::Ipv4 => {}
+        _ => return Ok(TC_ACT_PIPE),
+    }
+
+    let ipv4hdr: Ipv4Hdr = ctx.load(EthHdr::LEN).map_err(|_| ())?;
+    let destination = u32::from_be(ipv4hdr.dst_addr);
+
+    let action = if block_ip(destination) {
+        TC_ACT_SHOT
+    } else {
+        TC_ACT_PIPE
+    };
+
+    info!(&ctx, "DEST {:i}, ACTION {}", destination, action);
+
+    Ok(action)
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
---
+++ abd-bpf/src/abd_writer.rs
@@ -0,0 +1,73 @@
+use std::net::Ipv4Addr;
+
+use aya::{
+    maps::HashMap,
+    programs::{tc, SchedClassifier, TcAttachType},
+};
+use aya::EbpfLoader;
+use clap::Parser;
+use log::info;
+use log::{debug, warn};
+use std::env;
+use tokio::signal;
+
+/// A TC program which implements an ABD writer
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Network interface to attach the XDP program to
+    #[arg(short, long, default_value = "eth0")]
+    iface: String,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let Args { iface } = args;
+
+    env_logger::init();
+
+    // Bump the memlock rlimit. This is needed for older kernels that don't use the
+    // new memcg based accounting, see https://lwn.net/Articles/837122/
+    let rlim = libc::rlimit {
+        rlim_cur: libc::RLIM_INFINITY,
+        rlim_max: libc::RLIM_INFINITY,
+    };
+    let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
+    if ret != 0 {
+        debug!("remove limit on locked memory failed, ret is: {}", ret);
+    }
+
+    // This will include your eBPF object file as raw bytes at compile-time and load it at
+    // runtime. This approach is recommended for most real-world use cases. If you would
+    // like to specify the eBPF program at runtime rather than at compile-time, you can
+    // reach for `Bpf::load_file` instead.
+    let mut ebpf = EbpfLoader::new()
+        .load(aya::include_bytes_aligned!(concat!(
+            env!("OUT_DIR"),
+            "/abd-writer"
+        )))?;
+    if let Err(e) = aya_log::EbpfLogger::init(&mut ebpf) {
+        // This can happen if you remove all log statements from your eBPF program.
+        warn!("failed to initialize eBPF logger: {}", e);
+    }
+    // error adding clsact to the interface if it is already added is harmless
+    // the full cleanup can be done with 'sudo tc qdisc del dev eth0 clsact'.
+    let _ = tc::qdisc_add_clsact(&iface);
+    let program: &mut SchedClassifier = ebpf.program_mut("abd_writer").unwrap().try_into()?;
+    program.load()?;
+    program.attach(&iface, TcAttachType::Egress)?;
+
+    let mut blocklist: HashMap<_, u32, u32> =
+        HashMap::try_from(ebpf.map_mut("BLOCKLIST").unwrap())?;
+
+    let block_addr: u32 = Ipv4Addr::new(1, 1, 1, 1).into();
+
+    blocklist.insert(block_addr, 0, 0)?;
+
+    info!("Waiting for Ctrl-C...");
+    signal::ctrl_c().await?;
+    info!("Exiting...");
+
+    Ok(())
+}
